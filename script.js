08c73051a47cffb6c4163a69735802d53c3b0e22
Mystic Valley <mysticvalleyorg@gmail.com>
2024-10-17 15:50:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const z = async () => {
+  await delay(5000);
+  console.log("Waited 5s");
+
+  await delay(5000);
+  console.log("Waited an additional 5s");
+};
 let x = Number(prompt("Pick a number"));
 if (!Number.isNaN(x)) {
 x = "Your number is the square root of " + x * x;
