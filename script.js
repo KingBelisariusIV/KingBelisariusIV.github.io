9e137eb613d9af109c8c399e2025902fcbbe165e
Mystic Valley <mysticvalleyorg@gmail.com>
2024-10-17 15:55:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,5 @@ x = "Your number is the square root of " + x * x;
 document.querySelector('#header').innerHTML = x
   console.log("Your number is the square root of " + x * x);
 } else {
-  let y = "Hey! Why didn't you pick a number";
-  document.querySelector('#header').innerHTML = y;
 console.log("Hey! Why didn't you pick a number")
 }
